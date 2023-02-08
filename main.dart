09e57c8fd4617987cd3120ed360aa6b311ff8d2b
Abdulwahed Mansour <120674623+abd-mansour@users.noreply.github.com>
2023-02-08 10:25:07 +0200

    The full code Using HttpClient

---
+++ main.dart
@@ -0,0 +1,86 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'dart:io'; // for using HttpClient
+import 'dart:convert'; // for using json.decode()
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // The list that contains information about photos
+  List _loadedPhotos = [];
+
+  // The function that fetches data from the API
+  Future<void> _fetchData() async {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/photos';
+
+    HttpClient client = HttpClient();
+    client.autoUncompress = true;
+
+    final HttpClientRequest request = await client.getUrl(Uri.parse(apiUrl));
+    request.headers
+        .set(HttpHeaders.contentTypeHeader, "application/json; charset=UTF-8");
+    final HttpClientResponse response = await request.close();
+
+    final String content = await response.transform(utf8.decoder).join();
+    final List data = json.decode(content);
+
+    setState(() {
+      _loadedPhotos = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Kindacode.com'),
+        ),
+        body: SafeArea(
+            child: _loadedPhotos.isEmpty
+                ? Center(
+                    child: ElevatedButton(
+                      onPressed: _fetchData,
+                      child: const Text('Load Photos'),
+                    ),
+                  )
+                // The ListView that displays photos
+                : ListView.builder(
+                    itemCount: _loadedPhotos.length,
+                    itemBuilder: (BuildContext ctx, index) {
+                      return ListTile(
+                        leading: Image.network(
+                          _loadedPhotos[index]["thumbnailUrl"],
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        title: Text(_loadedPhotos[index]['title']),
+                        subtitle:
+                            Text('Photo ID: ${_loadedPhotos[index]["id"]}'),
+                      );
+                    },
+                  )));
+  }
+}
