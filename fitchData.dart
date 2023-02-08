55a8127f7a8bae3bbe1a9d2c202c06a999470985
Abdulwahed Mansour <120674623+abd-mansour@users.noreply.github.com>
2023-02-08 10:21:53 +0200

    Create fitchData.dart

---
+++ fitchData.dart
@@ -0,0 +1,20 @@
+// The function that fetches data from the API:
+
+Future<void> _fetchData() async {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/photos';
+
+    HttpClient client = HttpClient();
+    client.autoUncompress = true;
+
+    final HttpClientRequest request = await client.getUrl(Uri.parse(apiUrl));
+    request.headers
+        .set(HttpHeaders.contentTypeHeader, "application/json; charset=UTF-8");
+    final HttpClientResponse response = await request.close();
+
+    final String content = await response.transform(utf8.decoder).join();
+    final List data = json.decode(content);
+
+    setState(() {
+      _loadedPhotos = data;
+    });
+}
