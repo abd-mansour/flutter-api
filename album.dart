ddc2388ba512a42dc18caec29b59aad45257434c
Abdulwahed Mansour <120674623+abd-mansour@users.noreply.github.com>
2023-02-16 00:21:22 +0200

    Create album.dart

---
+++ album.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<dynamic> _users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  Future<void> _getUsers() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    if (response.statusCode == 200) {
+      setState(() {
+        _users = json.decode(response.body);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JSONPlaceholder Users',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('JSONPlaceholder Users'),
+        ),
+        body: ListView.builder(
+          itemCount: _users.length,
+          itemBuilder: (context, index) {
+            final user = _users[index];
+            return ListTile(
+              title: Text(user['name']),
+              subtitle: Text(user['email']),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
